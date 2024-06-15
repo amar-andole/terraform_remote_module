53cefe66694dea380b44cee941db958ece0b24a2
amar-andole <159524627+amar-andole@users.noreply.github.com>
2024-06-15 20:09:11 +0530

    Update main.tf

--- remote-exec/main.tf
+++ remote-exec/main.tf
@@ -16,7 +16,7 @@ provisioner "remote-exec" {
        "sudo chmod a+r /etc/apt/keyrings/docker.asc",
        "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo \"$VERSION_CODENAME\") stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
        "sudo apt-get update -y",
-       "sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y"
+       "sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
      ]
 }
 }
